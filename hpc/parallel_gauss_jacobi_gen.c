dd1b1843457f00c321271c03b9ebf5a03ab4c9e9
Devansh1106 <devansh22@iisertvm.ac.in>
2026-02-14 23:57:34 +0530

    Parallel Jacobi with n % size as anything

--- hpc/parallel_gauss_jacobi_gen.c
+++ hpc/parallel_gauss_jacobi_gen.c
@@ -1,13 +1,11 @@
 // parallel gauss-jacobi method (when n % size can be anything)
 
-// 23 / 4 = 6 6 6 5; rem = 23 % 4 = 3; local_n = ceil(23/4) + (rank < rem ? 1 : 0) 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <mpi.h>
 
-double* matrix_vec_prod(double* A, double* vec, int n, double* result_vec, int size, MPI_Comm comm);
+double* matrix_vec_prod(double* A, double* vec, int n, double* result_vec, int rank, int size, MPI_Comm comm);
 void gauss_jacobi(double* A, double* rhs_vec, double* initial_guess, int n, int max_iter, double tol, int rank, int size, MPI_Comm comm);
 
 
@@ -28,13 +26,12 @@ int main(int argc, char **argv)
     int max_iter = 10000;
     // Load balancing
     int rem = n % size;
-    int local_n = ceil(n/size) + (rank < rem ? 1 : 0);
+    int local_n = (n/size) + (rank < rem ? 1 : 0);
 
     if (rank == 0){
         A = malloc(n * n * sizeof(double));
         rhs_vec = malloc(n * sizeof(double));
         initial_guess = calloc(n, sizeof(double)); // 0 as initial guess
-        // sol = calloc(n, sizeof(double));
 
         for(size_t i = 0; i < n; i++){
             for(size_t j = 0; j < n; j++){
@@ -49,7 +46,6 @@ int main(int argc, char **argv)
         A = malloc(local_n * n * sizeof(double));
         rhs_vec = malloc(n * sizeof(double));
         initial_guess = calloc(n, sizeof(double));
-        // sol = calloc(n, sizeof(double));
         gauss_jacobi(A, rhs_vec, initial_guess, n, max_iter, max_tol, rank, size, comm);
     }
     free(A);
@@ -65,48 +61,56 @@ void gauss_jacobi(double* A, double* rhs_vec, double* initial_guess, int n, int 
     int iter = 0.0;
     double tol = 1.0;
     double sum = 0.0;
-    int* sendcount=NULL;
-    int* displs=NULL; 
+    int* sendcount1 =NULL;
+    int* displs1    =NULL;
+    int* sendcount2 =NULL;
+    int* displs2    =NULL; 
     double* result_vec=NULL;
     int flag = -1;
     double* diag=NULL;
     double* temp=NULL;
     
     // Load balancing
-    int rem = n % size;
-    int local_n = ceil(n/size) + (rank < rem ? 1 : 0);
+    int rem     = n % size;
+    int local_n = (n/size) + (rank < rem ? 1 : 0);
 
-    diag = malloc(n * sizeof(double));
-    result_vec = calloc(local_n, sizeof(double));
-    temp = calloc(local_n, sizeof(double));
-    sendcount = malloc(size * sizeof(int));
-    displs = malloc(size * sizeof(int));
+    diag        = malloc(n * sizeof(double));
+    result_vec  = calloc(local_n, sizeof(double));
+    temp        = calloc(local_n, sizeof(double));
+    sendcount1  = malloc(size * sizeof(int));
+    displs1     = malloc(size * sizeof(int));
+    sendcount2  = malloc(size * sizeof(int));
+    displs2     = malloc(size * sizeof(int));
 
     if (rank == 0){
         for(size_t i = 0; i < n; i++){
-            diag[i] = A[i*n + i];
-            A[i*n + i] = 0.0;
+            diag[i]     = A[i*n + i];
+            A[i*n + i]  = 0.0;
         }    
     }
     for(size_t i = 0; i < size; i++){
-        sendcount[i] = local_n*n;
+        int count = (n/size) + (i < rem ? 1 : 0);
+        sendcount1[i] = count * n;
+        sendcount2[i] = count;
+        displs1[i] = (i == 0) ? 0 : displs1[i - 1] + sendcount1[i - 1];
+        displs2[i] = (i == 0) ? 0 : displs2[i - 1] + sendcount2[i - 1];
     }
 
     start_time = MPI_Wtime();
-    MPI_Scatter(A, (local_n * n), MPI_DOUBLE, A, (local_n * n), MPI_DOUBLE, 0, comm);
-    MPI_Scatter(diag, local_n, MPI_DOUBLE, diag, local_n, MPI_DOUBLE, 0, comm);
+    MPI_Scatterv(A, sendcount1, displs1, MPI_DOUBLE, A, (local_n * n), MPI_DOUBLE, 0, comm);
+    MPI_Scatterv(diag, sendcount2, displs2, MPI_DOUBLE, diag, local_n, MPI_DOUBLE, 0, comm);
     MPI_Bcast(rhs_vec, n, MPI_DOUBLE, 0, comm);
     MPI_Bcast(initial_guess, n, MPI_DOUBLE, 0, comm); 
 
     while((tol > max_tol) && (iter != max_iter)){
         sum = 0.0;
-        result_vec = matrix_vec_prod(A, initial_guess, n, result_vec, size, comm);
+        result_vec = matrix_vec_prod(A, initial_guess, n, result_vec, rank, size, comm);
         for (size_t i = 0; i < local_n; i++){
             temp[i] = rhs_vec[rank*local_n + i] - result_vec[i];
             temp[i] = temp[i] / diag[i];
-            sum += fabs(temp[i] - initial_guess[rank*local_n + i]);
+            sum     += fabs(temp[i] - initial_guess[rank*local_n + i]);
         }
-        MPI_Allgather(temp, local_n, MPI_DOUBLE, initial_guess, local_n, MPI_DOUBLE, comm);
+        MPI_Allgatherv(temp, local_n, MPI_DOUBLE, initial_guess, sendcount2, displs2, MPI_DOUBLE, comm);
         MPI_Allreduce(&sum, &tol, 1, MPI_DOUBLE, MPI_SUM, comm);
         printf("\nTol: %f \t Rank:\t%d", tol, rank);
         printf("\t Iter: \t%d\n", iter);
@@ -132,8 +136,9 @@ void gauss_jacobi(double* A, double* rhs_vec, double* initial_guess, int n, int 
     // return initial_guess;
 }
 
-double* matrix_vec_prod(double* A, double* vec, int n, double* result_vec, int size, MPI_Comm comm){
-    int local_n = n/size;            // n % size should be 0 for now
+double* matrix_vec_prod(double* A, double* vec, int n, double* result_vec, int rank, int size, MPI_Comm comm){
+    int rem = n % size;
+    int local_n = (n/size) + (rank < rem ? 1 : 0);           // n % size should be 0 for now
     // reseting is needed otherwise it will have the value from the previous call
     for(size_t i = 0; i < local_n; i++) {
         result_vec[i] = 0.0;
